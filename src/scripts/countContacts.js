f1895a69ceaa0d0069fd3e364726650a297f298a
AntonK33 <antonkulackov@gmail.com>
2025-03-15 17:42:10 +0100

    hw1compleated

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,23 @@
-export const countContacts = async () => {};
+import { writeFile, readFile } from "fs/promises";
+import { PATH_DB } from "../constants/contacts.js";
 
-console.log(await countContacts());
+
+export const countContacts = async () => { 
+    let allContacts = [];
+    try {
+        const data = await readFile(PATH_DB, "utf-8");
+      allContacts = JSON.parse(data); 
+    } catch (error) {
+         console.error("Ошибка чтения файла:", error);
+    }
+
+    const countContacts = allContacts.length;
+
+    return countContacts;
+};
+
+countContacts().then(contacts => {
+        console.log("Контакты:", contacts);
+ });
+
+//console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,19 @@
-export const getAllContacts = async () => {};
+import { readContacts } from "../utils/readContacts.js";
+import { writeFile, readFile } from "fs/promises";
+import { PATH_DB } from "../constants/contacts.js";
 
-console.log(await getAllContacts());
+export const getAllContacts = async () => {
+   
+    try {
+        // const allContacts = await readContacts();
+        const allContacts = await readFile(PATH_DB, "utf-8");
+        return JSON.parse(allContacts);
+    } catch (error) {
+        console.error("Ошибка чтения файла:", error);
+         return [];
+    }
+    
+       
+ };
+getAllContacts().then(contacts => console.log(contacts));;
+//console.log(await getAllContacts());
